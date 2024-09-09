98a934211269bc1e752163a101b309fffef1c6c3
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-09-09 12:46:16 +0200

    adding client/server kafka relations

--- terraform/kafka/base/integrations.tf
+++ terraform/kafka/base/integrations.tf
@@ -28,3 +28,31 @@ resource "juju_integration" "kafka_admin" {
     endpoint = "kafka-client"
   }
 }
+
+resource "juju_integration" "kafka_producer" {
+  model      = var.model
+
+  application {
+    name = juju_application.producer.name
+    endpoint = "kafka-cluster"
+  }
+
+  application {
+    name = juju_application.kafka.name
+    endpoint = "kafka-client"
+  }
+}
+
+resource "juju_integration" "kafka_consumer" {
+  model      = var.model
+
+  application {
+    name = juju_application.spark_streaming.name
+    endpoint = "kafka"
+  }
+
+  application {
+    name = juju_application.kafka.name
+    endpoint = "kafka-client"
+  }
+}
